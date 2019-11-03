d8213b5b353b7c78f12b456d60faf0209a9692cb
Тараскин Михаил <MITaraskin@domclick.ru>
2019-11-03 13:33:26 +0300

    Visual studio build tools setup

--- bin/build.ps1
+++ bin/build.ps1
@@ -8,3 +8,12 @@ function Unzip
 
 Invoke-WebRequest -UseBasicParsing -OutFile rubyc.zip -Uri http://enclose.io/rubyc/rubyc-x64.zip
 Unzip "rubyc.zip" "rubyc"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise"
+# By Default github action windows env does not set up build tools, call VsDevCmd build tools
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+# Test nmake
+nmake -help
+.\rubyc\rubyc-v0.4.0-x64.exe --clean-tmpdir -o build\metanorma bin\metanorma || echo 'skip'
+# Check build folder
+dir C:/Users/RUNNER~1/AppData/Local/Temp/rubyc/zlib
