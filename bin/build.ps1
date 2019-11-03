ac38e00c68780206a78364b8f7bb65bc18eef76b
Тараскин Михаил <MITaraskin@domclick.ru>
2019-11-03 10:47:28 +0300

    Pwoershell script for build

---
+++ bin/build.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Invoke-WebRequest -UseBasicParsing -OutFile rubyc.zip -Uri http://enclose.io/rubyc/rubyc-x64.zip
+Unzip "rubyc.zip" "rubyc"
+.\rubyc\rubyc-v0.4.0-x64.exe
