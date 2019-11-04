4fe1f80f21bab40f2944b69db29890d18ba2815a
Mikl T <roberar2ua@gmail.com>
2019-11-04 10:09:48 +0300

    Fix ssl request certificate error (#13)
    * Embed mozila ssl root certificate into compiled binary in order to fix `certificate verify failed` error

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ -d build ] || mkdir build; rm -rf build/* || true
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export TEMP_DIR="$(mktemp -d)"
+else
+  export TEMP_DIR="$(mktemp -d --tmpdir=$HOME .rubyc-build.XXXXXX)"
+fi
+
+echo $TEMP_DIR
+cp Gemfile* $TEMP_DIR && cp bin/metanorma $TEMP_DIR && cp -R vendor $TEMP_DIR
+
+echo $TEMP_DIR
+./rubyc --clean-tmpdir -r $TEMP_DIR -o ./build/metanorma $TEMP_DIR/metanorma
