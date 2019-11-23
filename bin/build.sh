3644b1d122c6bd1eaedb721b8059d787e6165fcb
Тараскин Михаил <MITaraskin@domclick.ru>
2019-11-23 17:23:38 +0300

    Release v0.0.9: - linux compilation - Fix check for linux platform

--- bin/build.sh
+++ bin/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+# Set strict mode
+set -eu
 
 [ -d build ] || mkdir build; rm -rf build/* || true
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  export TEMP_DIR="$(mktemp -d)"
-else
-  export TEMP_DIR="$(mktemp -d --tmpdir=$HOME .rubyc-build.XXXXXX)"
-fi
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     export TEMP_DIR="$(mktemp -d --tmpdir=$HOME .rubyc-build.XXXXXX)";;
+    Darwin*)    export TEMP_DIR="$(mktemp -d)";;
+esac
 
 echo $TEMP_DIR
 cp Gemfile* $TEMP_DIR && cp bin/metanorma $TEMP_DIR && cp -R vendor $TEMP_DIR
-
-echo $TEMP_DIR
 ./rubyc --clean-tmpdir -r $TEMP_DIR -o ./build/metanorma $TEMP_DIR/metanorma
