31d5069a24e505f122cc02644eb6a7ec339d4c0e
Alexande B <abobrikovich@gmail.com>
2020-04-07 21:17:57 +0300

    Install bundler if missed

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,10 @@ esac
 
 echo TEMP_DIR=$TEMP_DIR GITHUB_REF=$GITHUB_REF
 
-[[ ${GITHUB_REF:-} == refs/tags/v* ]] && bundle install --jobs 4 --retry 3
+[[ ${GITHUB_REF:-} == refs/tags/v* ]] && {
+	sudo gem install bundler -v 1.17.3
+	bundle install --jobs 4 --retry 3
+}
 
 cp Gemfile* $TEMP_DIR && cp bin/metanorma $TEMP_DIR && cp -R vendor $TEMP_DIR
 ./rubyc --clean-tmpdir -r $TEMP_DIR -o ./build/metanorma $TEMP_DIR/metanorma
