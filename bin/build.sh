a1e5f30e24b1e8c04a0966101dc1a2de6bc336ab
Alexande B <abobrikovich@gmail.com>
2020-04-07 20:20:44 +0300

    fix if condition in bash

--- bin/build.sh
+++ bin/build.sh
@@ -10,9 +10,9 @@ case "${unameOut}" in
     Darwin*)    export TEMP_DIR="$(mktemp -d)";;
 esac
 
-echo $TEMP_DIR
+echo TEMP_DIR=$TEMP_DIR GITHUB_REF=$GITHUB_REF
 
-[[ ${GITHUB_REF} == refs/tags/v* ]] && bundle install --jobs 4 --retry 3
+[[ ${GITHUB_REF:-} == refs/tags/v* ]] && bundle install --jobs 4 --retry 3
 
 cp Gemfile* $TEMP_DIR && cp bin/metanorma $TEMP_DIR && cp -R vendor $TEMP_DIR
 ./rubyc --clean-tmpdir -r $TEMP_DIR -o ./build/metanorma $TEMP_DIR/metanorma
