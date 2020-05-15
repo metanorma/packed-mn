70acbd280882269eb275a6eb3e9f2be54bd1fb9e
Alexande B <abobrikovich@gmail.com>
2020-05-16 00:19:47 +0300

    #31 Add make test command

---
+++ bin/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CLONE_DIR=$(pwd)/build
+PROCESSORS=( iso cc gb iec itu ogc un iho nist )
+
+ERRORS=( )
+
+for s in "${PROCESSORS[@]}"; do \
+	[[ -d ${CLONE_DIR}/${s} ]] || git clone --recurse-submodules https://${GITHUB_CREDENTIALS}@github.com/metanorma/mn-samples-${s} ${CLONE_DIR}/${s}
+	pushd ${CLONE_DIR}/${s}
+		env PATH="$(pwd)/build:${PATH}" make all &> ${CLONE_DIR}/test_${s}.log
+		[ $? -ne 0 ] && ERRORS+=("${s}"); \
+	popd
+done; \
+if [ ${#ERRORS[@]} -ne 0 ]; then
+	for s in "${ERRORS[@]}"; do \
+		echo "--------------- tail -50 test_${s}.log ------------------"
+		tail -50 ${CLONE_DIR}/test_${s}.log
+	done
+	echo "------------------------------------------------------------"
+	echo "Failed processors (${ERRORS[@]}) check details above"
+	exit 1
+fi
