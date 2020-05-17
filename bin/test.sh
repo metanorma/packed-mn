f32031841f4fc85a3179930131a93a1318685bc9
Alexande B <abobrikovich@gmail.com>
2020-05-17 12:09:29 +0300

    Install yq

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 CLONE_DIR=$(pwd)/build
 PROCESSORS=( iso cc gb iec itu ogc un iho nist )
@@ -8,7 +9,7 @@ ERRORS=( )
 for s in "${PROCESSORS[@]}"; do \
 	[[ -d ${CLONE_DIR}/${s} ]] || git clone --recurse-submodules https://${GITHUB_CREDENTIALS}@github.com/metanorma/mn-samples-${s} ${CLONE_DIR}/${s}
 	pushd ${CLONE_DIR}/${s}
-		env PATH="$(pwd)/build:${PATH}" make all &> ${CLONE_DIR}/test_${s}.log
+		env PATH="${CLONE_DIR}:${PATH}" make all &> ${CLONE_DIR}/test_${s}.log
 		[ $? -ne 0 ] && ERRORS+=("${s}"); \
 	popd
 done; \
