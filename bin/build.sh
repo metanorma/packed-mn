982084c1de323f670e3c1616175a2c6d2e9152aa
Alexande B <abobrikovich@gmail.com>
2021-04-06 19:20:06 +0300

    Fix MKTEMP_OPTS

--- bin/build.sh
+++ bin/build.sh
@@ -9,10 +9,11 @@ set -a
 . "bin/$(uname -s).env"
 set +a
 
-TEMP_DIR="$(mktemp -d ${MKTEMP_OPTS:-})"
+TEMP_DIR=$(${MKTEMP_CMD[@]})
 
 cp Gemfile* "$TEMP_DIR"
 cp bin/metanorma "${TEMP_DIR}"
 cp -R vendor "$TEMP_DIR"
 
 ./rubyc -r "${TEMP_DIR}" -o ${BUILD_DIR}/metanorma "${TEMP_DIR}/metanorma" --clean-tmpdir 
+strip ${BUILD_DIR}/metanorma
