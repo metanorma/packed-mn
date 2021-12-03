9822212efbd78afe3f82bf08317a2dde97d9e548
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2021-12-03 15:25:50 +0300

    Enabled package step

---
+++ bin/build-with-tebako.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Set strict mode
+set -eu
+
+BUILD_DIR=${1:-./build}
+
+set -a
+# shellcheck disable=SC1090
+. "${BASH_SOURCE[0]%/*}/$(uname -s).env"
+set +a
+
+TEMP_DIR=$(${MKTEMP_CMD[@]})
+
+cp Gemfile* "$TEMP_DIR"
+cp bin/metanorma "${TEMP_DIR}"
+cp -R vendor "$TEMP_DIR"
+
+pushd tebako
+bin/tebako press -r "${TEMP_DIR}" -e "${TEMP_DIR}/metanorma" -o "${BUILD_DIR}/metanorma"
+popd
